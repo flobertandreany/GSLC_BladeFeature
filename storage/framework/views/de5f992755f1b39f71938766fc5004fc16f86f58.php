1c7e76c4b4ef90665b777e9f521d80bf957987a4
flobertandreany <117373338+flobertandreany@users.noreply.github.com>
2022-11-03 21:54:18 +0700

    Add files via upload

---
+++ storage/framework/views/de5f992755f1b39f71938766fc5004fc16f86f58.php
@@ -0,0 +1,70 @@
+<?php $__env->startSection('content'); ?>
+  <div class="container text-center">
+    <div class="row mt-3">
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/1/7/51d6ab93-e31e-4857-81d3-5e3cc1eaffe6.jpg" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">ASUS ROG STRIX-G G513IC RYZEN 7-4800H 8GB 512GB RTX3050 4GB OHS W10</h5>
+              <p class="card-text">Rp16.369.000</p>
+              <a href="#" class="btn btn-primary">Beli</a>
+            </div>
+          </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/7/25/b5d99b5f-b922-4a9d-81cf-a2451a483441.jpg" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Asus ROG Zephyrus M16 GU603ZW RTX3070Ti i9-12900H 40GB 2TB SSD 16" QHD</h5>
+              <p class="card-text">Rp34.599.000</p>
+              <a href="#" class="btn btn-primary">Beli</a>
+            </div>
+          </div>
+      </div>
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+            <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/6/21/cc122f80-9145-4462-b3e8-5f3e98617f89.jpg" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">ASUS ROG ZEPHYRUS DUO 16 GX650RM RYZEN 7-6800H 16GB RTX3060 6GB W11</h5>
+              <p class="card-text">Rp33.589.000</p>
+              <a href="#" class="btn btn-primary">Beli</a>
+            </div>
+          </div>
+      </div>
+    </div>
+    <div class="row mt-3">
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+              <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/6/11/154c44de-d262-4dd5-ac7e-6e9a27a690cd.jpg" class="card-img-top" alt="gambar laptop">
+              <div class="card-body">
+                <h5 class="card-title">Asus TUF FX507ZC-I735B6G-O | i7-12700H/512GB/8GB/RTX3050</h5>
+                <p class="card-text">Rp16.475.000</p>
+                <a href="#" class="btn btn-primary">Beli</a>
+              </div>
+            </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+              <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/3/8/8c54ffca-3232-4c29-ba9a-21239203370a.png" class="card-img-top" alt="gambar laptop">
+              <div class="card-body">
+                <h5 class="card-title">ASUS ROG STRIX SCAR 15 G533ZM I9-12900H 16GB 1TB RTX3060 300Hz W11 OHS</h5>
+                <p class="card-text">Rp28.899.000</p>
+                <a href="#" class="btn btn-primary">Beli</a>
+              </div>
+            </div>
+        </div>
+        <div class="col">
+          <div class="card" style="width: 18rem;">
+              <img src="https://images.tokopedia.net/img/cache/900/VqbcmM/2022/6/25/763bec5a-78fa-4493-b456-8566a82c0bcb.jpg" class="card-img-top" alt="gambar laptop">
+              <div class="card-body">
+                <h5 class="card-title">Asus ROG Flow X13 GV301RC Ryzen 7-6800HS RTX3050 16GB 512GB Win 11+OHS</h5>
+                <p class="card-text">Rp23.749.000</p>
+                <a href="#" class="btn btn-primary">Beli</a>
+              </div>
+            </div>
+        </div>
+      </div>
+  </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Ivan\Documents\Semester 5 Mata Kuliah\Web Programming\2440010731_GSLC Blade Feature\resources\views/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e600874f7b752d1222178c299234f2d3589d950b.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Jual laptop</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Order</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+<?php /**PATH C:\Users\Ivan\Documents\Semester 5 Mata Kuliah\Web Programming\2440010731_GSLC Blade Feature\resources\views/layout/navbar.blade.php ENDPATH**/ ?>
